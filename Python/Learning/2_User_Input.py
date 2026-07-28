751bdb81bcaf373e73526124d4ab1f07e2d2c9fc
Lalithesh Channu <lalithesh@Mac.lan>
2026-07-28 13:07:01 +0530

    Python Learning

---
+++ Python/Learning/2_User_Input.py
@@ -0,0 +1,12 @@
+"""
+user_input = input()
+print(user_input)
+"""
+
+
+"""
+user_input = input("Please enter your Name:")
+print("Hello", user_input)
+"""
+
+
---
+++ Python/Learning/4_Type_conversion.py
@@ -0,0 +1,5 @@
+num_int = 30
+num_float = num_int + 0.5
+print(num_float)
+print(type(num_float))
+
