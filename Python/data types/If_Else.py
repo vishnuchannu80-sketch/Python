7abb317b37161e0d6ec72c82b0144cb187fb2582
Lalithesh Channu <lalithesh@Mac.lan>
2026-08-07 13:37:08 +0530

    aug 8 If-Else

---
+++ Python/data types/If_Else.py
@@ -0,0 +1,16 @@
+user1 = "vysh"
+user2 = "vish"
+user3 = "ram"
+
+vysh = "torcher"
+vish = "peace"
+ram = "paapam pasivaadu"
+name = input("Enter your name: ")
+if name == user1:
+    print(f"Hello, {vysh}")
+elif name == user2:
+    print(f"Hello, {vish}")
+elif name == user3:
+    print(f"Hello, {ram}")
+else:
+    print("Hello, new user!")
